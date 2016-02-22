3c3735144ebd222dddc95cc5a95cd2e8bf374c82
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-02-22 10:10:29 +0100

    format

--- python/cython_impl.pyx
+++ python/cython_impl.pyx
@@ -1,14 +1,13 @@
+# http://stackoverflow.com/questions/7075082/what-is-future-in-python-used-for-and-how-when-to-use-it-and-how-it-works
 from __future__ import division
 import numpy as np
 cimport numpy as np
 from libc.math cimport sin, sqrt
-#from math import sin, sqrt
 
-#DTYPE_INT = np.int
+
 DTYPE_DBL = np.float64
-
-#ctypedef np.int_t DTYPE_INT_t
 ctypedef np.float64_t DTYPE_DBL_t
+
 
 def ex1(np.ndarray[DTYPE_DBL_t, ndim=1] a, np.ndarray[DTYPE_DBL_t, ndim=1] b):
     if a.shape[0] != b.shape[0]:
@@ -18,5 +17,5 @@ def ex1(np.ndarray[DTYPE_DBL_t, ndim=1] a, np.ndarray[DTYPE_DBL_t, ndim=1] b):
     cdef np.ndarray[DTYPE_DBL_t, ndim=1] c = np.zeros(size, dtype=DTYPE_DBL)
     cdef Py_ssize_t i
     for i in range(size):
-        c[i] = sin(2.2*a[i] - 3.3*b[i]) / sqrt(4.4*a[i] + 5.5*b[i])
+        c[i] = sin(2.2 * a[i] - 3.3 * b[i]) / sqrt(4.4 * a[i] + 5.5 * b[i])
     return c
--- python/python_impl.py
+++ python/python_impl.py
@@ -1,5 +1,7 @@
-import numpy as np
 from math import sin, sqrt
+
+import numpy as np
+
 
 def ex1(a, b):
     if a.shape[0] != b.shape[0]:
@@ -9,5 +11,5 @@ def ex1(a, b):
     size = a.shape[0]
     c = np.zeros(size, dtype=np.float64)
     for i in range(size):
-        c[i] = sin(2.2*a[i] - 3.3*b[i]) / sqrt(4.4*a[i] + 5.5*b[i])
+        c[i] = sin(2.2 * a[i] - 3.3 * b[i]) / sqrt(4.4 * a[i] + 5.5 * b[i])
     return c
