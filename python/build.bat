129eac945de9193cd9fe35b1f2855ca02206a4c2
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-02-23 10:41:07 +0100

    fix for visiual studio 14 (15?)

--- python/build.bat
+++ python/build.bat
@@ -1,4 +1,7 @@
-SET SDK_SETENV=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
-SET VS100COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\
+SET MSVS14=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+SET VS140COMNTOOLS=%MSVS14%\Common7\Tools\
+SET VS100COMNTOOLS=%VS140COMNTOOLS%
+
+%MSVS14%\vcvarsall.bat amd64
 
 python setup.py build_ext --inplace
